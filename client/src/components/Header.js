6a98e33f13252286661a961894e872ee33e647f1
Spencer Fife <scottyfife17@gmail.com>
2020-05-14 11:05:46 -0700

    button, header and navbar components

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from `react`;
+
+class Header extends React.Component {
+    render () {
+        return (
+            <h1 className="jumbotron">Google Book Search</h1>
+        )
+    };
+}
+
+export default Header;
