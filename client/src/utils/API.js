7e95cd6571097372ad04d8f896fd833edce7009b
Spencer Fife <scottyfife17@gmail.com>
2020-05-14 10:51:10 -0700

    edit book model, add axios routes, add seed file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,19 @@
 import axios from "axios";
 
 export default {
-  //axios functions for searching (get), saving (put), and deleting (delete) books from book list
+  //axios functions for searching Google/book list (get), saving (post), and deleting (delete) books from book list
+  getGoogleResults(userInput) {
+    return axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${userInput}`
+    );
+  },
+  getBooks() {
+    return axios.get(`/api/books`);
+  },
+  deleteBook(id) {
+    return axios.delete(`/api/books/${id}`);
+  },
+  saveBook(bookData) {
+    return axios.post(`/api/books`, bookData);
+  },
 };
--- models/book.js
+++ models/book.js
@@ -1,11 +1,11 @@
 const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 const bookSchema = new Schema({
-  authors: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  link: { type: String, required: true },
   title: { type: String, required: true },
+  authors: [{ type: String, required: true }],
+  image: { type: String, required: true },
+  description: { type: String, required: true },
+  link: { type: String, required: true },
 });
 
 const Book = mongoose.model(`Book`, bookSchema);
